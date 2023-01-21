4930483924caa893e077b206b0b92bcf09365aed
KotoricMensur <mensur.kotoric@edu.htl-klu.at>
2023-01-21 17:09:07 +0100

    Script for moving the player

---
+++ game/scripts/PlayerMovement.js
@@ -0,0 +1,41 @@
+let dDown = false;
+let aDown = false;
+let sDown = false;
+let speed = 2;
+
+// for jumping
+let currentJump = 0;
+let jumpHeight = 150;
+let jumpSpeed = 0.5;
+let activateJumping = false;
+let spaceDown = false;
+let spaceDownTemp = false;
+let jumpXValue = 2;
+
+function movePlayer(stepX, stepY) {
+    if (player.x + stepX >= 0 && player.y + stepY >= 0 && player.x + player.width + stepX <= gameArea.canvas.width && player.y + player.height + stepY <= gameArea.canvas.height) {
+        gameArea.clear();
+        player.x += stepX;
+        player.y += stepY;
+        player.update();
+    }
+}
+
+function moveLeft() {
+    movePlayer(-speed, 0);
+}
+
+function moveRight() {
+    movePlayer(speed, 0)
+}
+
+function moveDown() {
+    movePlayer(0, speed);
+}
+
+function jump() {
+    isGrounded = false;
+    currentJump -= jumpSpeed;
+    movePlayer(jumpXValue, currentJump);
+}
+

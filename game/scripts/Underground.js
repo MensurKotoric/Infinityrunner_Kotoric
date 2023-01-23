6572d8606a4a87bb257e2b17a28bcf2213f38e48
Dominik Georg Zidej (3CHEL) <dominik.zidej@edu.htl-klu.at>
2023-01-23 12:12:57 +0100

    implemented background-component

---
+++ game/scripts/Underground.js
@@ -0,0 +1,16 @@
+
+function underground(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.image = new Image();
+    this.image.src = color;
+    this.update = function(){
+        ctx = gameArea.context;
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+    }
+}
---
+++ game/scripts/UndergroundMove.js
@@ -0,0 +1,6 @@
+let myUnderground;
+
+function startMoving(){
+    myBackground = new underground(1870, 50, "styles/textures/textures_ground.png");
+    gameArea.start();
+}

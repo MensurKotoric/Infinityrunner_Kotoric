d922fe2298ea09f9433814a11bc690466942554f
KotoricMensur <mensur.kotoric@edu.htl-klu.at>
2023-02-04 18:53:51 +0100

    some simple changes

--- game/scripts/GenerateScore.js
+++ game/scripts/GenerateScore.js
@@ -3,14 +3,13 @@ let currentScore = 0;
 
 function showScore(){
     frameCounter++;
-    // increase timer every second (50Hz --> %50)
-    if(frameCounter%50 == 0){
+    // increase score every second (60Hz --> %60)
+    if(frameCounter%60 == 0){
         currentScore++;
     }
     scoreObj.text = "Score: " + currentScore;
     if(currentScore >= localStorage.getItem('highScore')){
         localStorage.setItem('highScore', currentScore);
-        //window.alert(localStorage.getItem('highScore'));
     }
     scoreObj.update();
 }

f6ce9c45342f83e18051abe6f4e8b8b3c1e18a6f
Dominik Georg Zidej (3CHEL) <dominik.zidej@edu.htl-klu.at>
2023-02-27 12:42:28 +0100

    implemented oop for Background.js and Underground.js

--- game/scripts/Background_Underground/Underground.js
+++ game/scripts/Background_Underground/Underground.js
@@ -1,18 +1,21 @@
-function underground(width, height, path, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = -speedOfObstacles;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.image = new Image();
-    this.image.src = path;
-    this.update = function () {
+class underground {
+    constructor(width, height, path, x, y)
+    {
+        this.width = width;
+        this.height = height;
+        this.speedX = -speedOfObstacles;
+        this.speedY = 0;
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.image.src = path;
+    }
+    update() {
         let ctx = gameArea.context;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 
     }
-    this.newPos = function () {
+    newPos() {
         this.speedX = -speedOfObstacles;
         this.x += this.speedX;
         if (this.x <= -this.width + 30) {

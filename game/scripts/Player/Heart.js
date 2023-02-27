d9f92a84e64c5684820877ddcbe11d7904f2f32f
Dominik Georg Zidej (3CHEL) <dominik.zidej@edu.htl-klu.at>
2023-02-27 12:38:37 +0100

    implemented oop for Heart.js

--- game/scripts/Player/Heart.js
+++ game/scripts/Player/Heart.js
@@ -1,11 +1,14 @@
-function heart(width, height, path, x, y){
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.image = new Image();
-    this.image.src = path;
-    this.update = function () {
+class heart {
+    constructor(width, height, path, x, y)
+    {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.image.src = path;
+    }
+    update() {
         let ctx = gameArea.context;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }

e02896f8cf23b73223b081cd1b9fee7be18e42f0
Dominik Georg Zidej (3CHEL) <dominik.zidej@edu.htl-klu.at>
2023-02-27 12:37:13 +0100

    implemented oop for Score.js

--- game/scripts/Player/Score.js
+++ game/scripts/Player/Score.js
@@ -1,14 +1,19 @@
-function score(width, height,color,x,y,type){
-    this.type = type;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.update = function() {
+class score {
+    constructor(width, height,color,x,y,type)
+    {
+        this.type = type;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.text = '';
+    }
+    update() {
         let panel = gameArea.context;
         if (this.type == "text") {
             panel.font = this.width + " " + this.height;
-            panel.fillStyle = color;
+            panel.fillStyle = this.color;
             panel.fillText(this.text, this.x, this.y);
         }
     }

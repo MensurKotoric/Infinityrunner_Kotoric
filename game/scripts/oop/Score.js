8fd68ed9abe777f6146ebebb371aa5105a39dc3e
Mensur Kotoric (3CHEL) <mensur.kotoric@edu.htl-klu.at>
2023-03-20 11:45:40 +0100

    implemented score script

---
+++ game/scripts/oop/Score.js
@@ -0,0 +1,15 @@
+class Score extends LabelObjects {
+    constructor(x, y, text, color, font,game) {
+        super(x, y, text, color, font,game);
+        this.frameCounter = 0;
+        this.currentScore = 0;
+    }
+    show(){
+        this.frameCounter++;
+        if(this.frameCounter % 50 == 0){
+            this.currentScore++;
+        }
+        this.text = "Score: " + this.currentScore;
+        this.update();
+    }
+}

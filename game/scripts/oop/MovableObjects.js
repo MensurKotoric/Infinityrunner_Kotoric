36f7f08f3483506aac0a0235fe94633485d5929c
Mensur Kotoric (3CHEL) <mensur.kotoric@edu.htl-klu.at>
2023-03-20 11:44:23 +0100

    implemented movable object script

---
+++ game/scripts/oop/MovableObjects.js
@@ -0,0 +1,25 @@
+class MovableObjects extends StandardObject {
+    constructor(x, y, width, height, identifier, stepX, stepY, game) {
+        super(x, y, width, height, identifier, game);
+        this.stepX = stepX;
+        this.stepY = stepY;
+    }
+
+    move() {
+        this.x += this.stepX;
+        this.y += this.stepY;
+        this.update();
+    }
+
+    checkCollision(object) {
+        return this.checkPoint(this.x, this.y, object.x, object.y, object.width, object.height) ||
+            this.checkPoint(this.x + this.width, this.y, object.x, object.y, object.width, object.height) ||
+            this.checkPoint(this.x, this.y + this.height, object.x, object.y, object.width, object.height) ||
+            this.checkPoint(this.x + this.width, this.y + this.height, object.x, object.y, object.width, object.height);
+    }
+
+    checkPoint(pX, pY, oX, oY, oW, oH) {
+        return pX >= oX && pX <= oX + oW && pY >= oY && pY <= oY + oH;
+    }
+
+}
